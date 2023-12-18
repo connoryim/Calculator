71ad004589cb73b689701635b07af19eaaea37e9
connoryim <0134269333+connoryim@users.noreply.github.com>
2023-12-18 14:27:47 -0500

    Results are limited to only 2 decimal places

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function add(num1, num2) {
-    return num1 + num2;  
+    result = num1 + num2;  
+    return result.toFixed(2);
 };
 
 function subtract(num1, num2){
-    return Math.sign(num1-num2);
+    result = Math.sign(num1-num2);
+    return result.toFixed(2);
 };
 
 function multiply(num1,num2){
-    return num1*num2;
+    result = num1*num2;
+    return result.toFixed(2);
 };
 
 function divide(num1,num2){
