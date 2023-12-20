656d14578d28bfe09e2da88801bb6d87d976ad14
connoryim <0134269333+connoryim@users.noreply.github.com>
2023-12-19 21:32:13 -0500

    Operate takes equation string and calls approriate function

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function add(num1, num2) {
-    result = num1 + num2;  
-    return result.toFixed(2);
-};
+var num1 = undefined;
+var num2 = undefined;
+var oprtr = undefined;
 
+function add(num1, num2) {
+    var result = parseInt(num1) + parseInt(num2); 
+    result = result.toFixed(2);
+    console.log(result);
+    return result;
+
+};  
 function subtract(num1, num2){
     result = Math.sign(num1-num2);
     return result.toFixed(2);
@@ -16,4 +22,23 @@ function multiply(num1,num2){
 function divide(num1,num2){
     result = num1/num2;
     return result.toFixed(2);
+};
+
+
+function operate(string){
+    console.log(string);
+    var equation = string.split("");
+    var oprtr = equation[1];
+    var num1 = equation[0];
+    var num2 = equation[2];
+    console.log(oprtr);
+    if (oprtr = "+") {
+        return add(num1,num2);
+    } else if(oprtr ="-"){
+        subtract(num1,num2);
+    } else if(oprtr = "*"){
+        multiply(num1,num2);
+    } else if(oprtr ="/"){
+        divide(num1,num2);
+    }
 };
