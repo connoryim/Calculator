d5e24e433a0f392f75141dc20e3eac4c6c0b51b1
connoryim <0134269333+connoryim@users.noreply.github.com>
2024-01-13 02:10:43 -0500

    Dividing by zero will display an alert

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var oprtr = undefined;
 var result = undefined;
 
 
+
+//Functions used to perform the math
 function add(num1, num2) {
     result = parseInt(num1) + parseInt(num2); 
     result = parseFloat(result.toFixed(2));
@@ -34,32 +36,36 @@ var equation = "";
 //Operate splits the equation in the display and performs calls a function depening on the operator
 function operate(string){
     
-    equation = string.split(/([+,\-,/,x])/g);
-
+    equation = string.split(/([+,\-,÷,x])/g);
+    if(equation[equation.length-1] ==""){
+        equation = equation.slice(0,-2);
+        console.log(equation);
+    };
+    //Checks array for multiplication and divison
     for (let i = 0; i < equation.length; i++) {
         if(equation[i] == "x"){
             equation[i-1] = multiply(equation[i-1],equation[i+1]);
             equation.splice(i,2);
-            console.log(equation);
             i=0;
-        } else if(equation[i] == "/"){
+        } else if(equation[i] == "÷"){
+            if(equation[i+1] == 0){
+                alert("Sorry can't do that...");
+                break;
+            }
             equation[i-1] = divide(equation[i-1],equation[i+1]);
             equation.splice(i,2);
-            console.log(equation);
             i=0;
         } 
     }
-
+    //Checks array for addition and subtraction
     for (let i = 0; i < equation.length; i++){
         if(equation[i] == "+"){
             equation[i-1] = add(equation[i-1],equation[i+1]);
             equation.splice(i,2);
-            console.log(equation);
             i=0;
         } else if(equation[i] == "-"){
             equation[i-1] = subtract(equation[i-1],equation[i+1]);
             equation.splice(i,2);
-            console.log(equation);
             i=0;
         } 
     };
@@ -96,6 +102,8 @@ equals.addEventListener("click",() =>{
     display.textContent = operate(display.textContent);
 });
 
+
+//Removes the last value in the display string
 const del = document.querySelector("#del");
 var delCon = ""
 del.addEventListener("click",()=>{
